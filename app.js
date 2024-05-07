04ee987cb2df22423b28e4493dcbe3668bf069f9
cdevadhar <cdevadhar@gmail.com>
2024-05-06 22:52:30 -0700

    added routing

--- app.js
+++ app.js
@@ -1,48 +1,20 @@
 const express = require("express");
+const profileRoute = require("./routes/profile");
+const voteRoute = require("./routes/vote");
+const scoreboardRoute = require("./routes/scoreboard");
+const promptRoute = require("./routes/prompt");
 
 const app = express();
-
 app.use(express.json());
 
-// GET
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
-
-app.get("/scoreboard", (req, res) => {
-
-});
-
-app.get("/prompt", (req, res) => {
-
-});
-
-app.get("/vote", (req, res) => {
-
-});
-
-app.get("/profile", (req, res) => {
-
-});
-
-// POST
-
-app.post("/scoreboard", (req, res) => {
-    return res.redirect("/scoreboard");
-});
-
-app.post("/prompt", (req, res) => {
-    return res.redirect("/prompt");
-});
-
-app.post("/vote", (req, res) => {
-    return res.redirect("/vote");
-});
-
-app.post("/profile", (req, res) => {
-    return res.redirect("/profile");
-});
+app.use('/profile', profileRoute);
+app.use("/vote", voteRoute);
+app.use('/scoreboard', scoreboardRoute);
+app.use("/prompt", promptRoute);
 
 // start app
 app.listen("3000", () => console.log("Server started on port 3000"));
---
+++ routes/vote.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("Vote route");
+});
+
+router.post("/", (req, res) => {
+    return res.redirect("/");
+});
+
+module.exports = router;
