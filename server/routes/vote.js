1bb65def990c23d5ce97001b2719f97d03d7ff85
Stewart Kwok <stewart.kwok1@gmail.com>
2024-05-20 23:56:51 -0700

    Add voting behavior
    Authentication not implemented yet, but should be able to add votes with post

--- server/routes/vote.js
+++ server/routes/vote.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const Response = require("../models/responseModel.js");
 
-router.get("/", (req, res) => {
-    res.send("Vote route");
-});
+router.post("/", async (req, res) => {
 
-router.post("/", (req, res) => {
-    return res.redirect("/vote");
+    if (true){ // later, when authenticated, check for authentication
+        let inc_amt = 1;
+        if (false) { // for later, when unvoting, check whether user has voted for already (needs authentication first)
+            inc_amt = -1;
+        }
+
+        if (!req.body.response_id){
+            res.status(400).send("Missing response_id");
+        }
+
+        const response = await Response.findByIdAndUpdate(req.body.response_id, { $inc : {"votes" : inc_amt}}, {"new" : true});
+
+        return res.send(response);
+    }
 });
 
 module.exports = router;
