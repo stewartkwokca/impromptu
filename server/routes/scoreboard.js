34291126178df53ec85f0f38d618497297bedee8
stewartkwokca <stewart.kwok1@gmail.com>
2024-05-29 13:09:39 -0700

    scoreboard now queries for responses made on current date only

--- server/routes/scoreboard.js
+++ server/routes/scoreboard.js
@@ -5,7 +5,11 @@ const Response = require("../models/responseModel.js");
 let limit = 10; // change to show different number of responses on scoreboard
 
 router.get("/", async (req, res) => {
-    const responses = await Response.find().sort({votes: -1}).limit(limit);
+    const today = new Date();
+    const mm = today.getMonth()+1;
+    const dd = today.getDate();
+    const yyyy = today.getFullYear();
+    const responses = await Response.find({createdAt: { "$gte": `${yyyy}-${mm}-${dd}` }}).sort({votes: -1}).limit(limit);
     return res.json({responses: responses});
 });
 
