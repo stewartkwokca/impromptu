959cb184c6066c0d94a779890491566f38901179
cdevadhar <cdevadhar@gmail.com>
2024-05-29 14:15:12 -0700

    added pages, keeping track of views

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -4,13 +4,24 @@ const router = express.Router();
 const Response = require("../models/responseModel.js");
 const User = require('../models/userModel');
 
+function getPage (responses, page) {
+    if (responses.length>10) {
+        if (page*10>responses.length) {
+            responses = [];
+        }
+        responses = responses.slice(page*10, page*10+10);
+    }
+    return responses;
+}
 router.get("/", async (req, res) => {
     const date = new Date();
     date.setHours(0, 0, 0, 0);
+    const page = req.query.page || 0;
     if (!req.session.userID) {
         // send default feed for non-logged in users
-        const responses = await Response.find({"createdAt": {$gte: date}});
+        const responses = getPage(await Response.find({"createdAt": {$gte: date}}), page);
         res.send({"responses": responses});
+        await Response.updateMany({"_id": {$in: responses}}, {$inc: {"views": 1}});
         return;
     }
     const user = await User.findOne({"_id": req.session.userID});
@@ -19,7 +30,12 @@ router.get("/", async (req, res) => {
         return;
     }
     const viewed = user.responsesViewed || [];
-    const responses = await Response.find({"createdAt": {$gte: date}, "_id": {$nin: viewed}});
+    const responses = getPage(await Response.find({"createdAt": {$gte: date}, "_id": {$nin: viewed}}), page);
+    for (const response in responses) {
+        viewed.push(response._id);
+    }
+    await User.updateOne({"_id": req.session.userID}, {$set: {"responsesViewed": viewed}});
+    await Response.updateMany({"createdAt": {$gte: date}}, {$inc: {"views": 1}});
     res.send(responses);
     return;
 
