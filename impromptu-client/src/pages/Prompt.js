554bf04d2b9a026d3f306a827020cfd353324be9
auspice <zhaokty@gmail.com>
2024-05-29 23:05:45 -0700

    Going to Prompt no longer causes RTE
    tops as a variable name makes no sense

--- impromptu-client/src/pages/Prompt.js
+++ impromptu-client/src/pages/Prompt.js
@@ -4,23 +4,18 @@ import React, { useState, useEffect } from 'react';
 const api_url = "http://localhost:8000";
 
 const Prompt = () => {
-    const [tops, setTops] = useState([]);
+    const [prompt, setPrompt] = useState("");
     const [editorContent, setEditorContent] = useState("Start editing...");
 
     useEffect(() => {
         axios.get(`${api_url}/prompt`).then((res, err) => {
             console.log(res);
-            setTops(res.data.prompt);
+            setPrompt(res.data.text);
         }).catch((err) => {
                 console.log(err);
             })
     }, [])
 
-    const renderItems = tops.map((content, index) => 
-        <div className="border rounded border-sky-300 first:text-amber-400 place-content-center">
-            <p className="mx-8 my-2">{content.response}</p>
-        </div>
-    );
 
     const formatText = (command, value = null) => {
         document.execCommand(command, false, value);
@@ -36,10 +31,8 @@ const Prompt = () => {
     return (
         <div className="center">
             <div className="place-content-center">
-                <h1 className="text-2xl font-bold text-center mb-10">Prompt</h1>
-            </div>
-            <div className="mx-20 grid grid-cols-2 gap-4 flex">
-                {renderItems}
+                <h1 className="text-2xl font-bold text-center mb-4">Prompt</h1>
+                <p className="text-lg text-center mb-10">{prompt}</p>
             </div>
             {/* Rich Text Editor */}
             <div className="toolbar">
