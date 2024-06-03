c1a1afb04fc5cb0c9bc2a0cc9a44e109b2312b84
Jake <umshmihle@gmail.com>
2024-06-02 18:25:36 -0700

    Added slider for voting page, need to to connect submission button to axios

--- impromptu-client/src/pages/Vote.js
+++ impromptu-client/src/pages/Vote.js
@@ -4,50 +4,90 @@ import { useState, useEffect } from 'react';
 const api_url = "http://localhost:8000"
 
 
+
 const Vote = ({user}) => {
     const [feed, setFeed] = useState([]);
     const [prompt, setPrompt] = useState("");
+    const [index, setIndex] = useState(0);
 
-    useEffect(() =>{
+    function getResponses() {
         axios.get(`${api_url}/feed`).then((res, err) => {
             console.log(res)
             setFeed(res.data.responses)
+            console.log(feed)
         }).catch((err) => {
             console.log(err)
         })
-    }, [])
-
-    useEffect(() => {
+    }
+    
+    function getPrompt(){
         axios.get(`${api_url}/prompt`).then((res, err) => {
             console.log(res)
             setPrompt(res.data.text)
         }).catch((err) => {
             console.log(err)
         })
-    }, [])
+    }
 
+    function setup(){
+        getPrompt()
+        getResponses()
+    }
 
+    useEffect(() => {
+        setup();
+    }, []);
 
-    const renderItems = feed.map((content) => 
-        <div className="border rounded border-sky-300 place-content-center">
-            <p className="mx-8 my-2 text-sm font-light">@{content.user}</p>
-            <p className="mx-8 my-2">{content.response}</p>
-            <form action="http://localhost:8000/vote" method="post">
-                <button class="mx-8 my-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Vote!</button>
-            </form>
+    const SliderComponent = () => {
+        const [value, setValue] = useState(5);
+    
+        const handleChange = (event) => {
+            setValue(event.target.value);
+        };
+    
+        return (
+            <div className="flex-grow">
+                <label htmlFor="slider" className="text-lg font-medium mr-10">
+                Rate Response:
+                </label>
+                <input
+                    type="range"
+                    id="slider"
+                    name="slider"
+                    min="0"
+                    max="10"
+                    value={value}
+                    onChange={handleChange}
+                    className="w-2/3"
+                />
+            </div>
+        );
+    }
+
+    function DisplayResponse(){
+        if (feed.length != 0){
+        return (
+            <div className="border rounded border-sky-300 p-4 w-2/3 mx-auto">
+            <p className="mx-8 my-2 text-sm font-light">@{feed[index].user}</p>
+            <p className="mx-8 my-2">{feed[index].response}</p>
+            <div className="flex w-full">
+            < SliderComponent/>
+            <button class=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Vote!</button>
+            </div>
         </div>
-    )
-
+        )
+        }
+    }
 
     return (
-        <div className="center">
+        <div>
             <h1 className="text-2xl font-bold text-center mb-4"> Vote Page </h1>
             <p className="text-lg text-center mb-10">{prompt}</p>
-            <div className="mx-20 grid grid-cols-2 gap-4 flex">
-                {renderItems}
-            </div>
+            <DisplayResponse content={feed[index]}/>
         </div>
     );
 }
+
+
 
 export default Vote;
