f9b820e3d567fa4e31c8b4eab470d37e8f6345f1
cdevadhar <cdevadhar@gmail.com>
2024-06-04 16:24:19 -0700

    feed fix (don't see the same response twice)

--- server/routes/vote.js
+++ server/routes/vote.js
@@ -25,7 +25,7 @@ router.post("/", async (req, res) => {
         if (response.usersVoted.includes(req.session.userID)) { console.log(response.usersVoted); return res.status(401).send("Can't vote twice a day")};
 
         response = await Response.findByIdAndUpdate(req.body.response_id, { $inc : {"votes" : req.body.votes}, $push: {"usersVoted" : req.session.userID}}, {"new" : true});
-
+        await User.findByIdAndUpdate(req.session.userID, {$push: {"responsesVoted": req.body.response_id}});
         return res.send(response);
     }
 });
