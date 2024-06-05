424bbd9ec72e5db7e0aa5d7fb6e2112beb62e18e
auspice <zhaokty@gmail.com>
2024-06-05 09:32:07 -0700

    one questionably fnny statement

--- server/routes/landing.js
+++ server/routes/landing.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/animation", (req, res) => {
-    let landingText = ["the gambling site for children", "it's like github for lesbians", "the child of myspace and wordle", "as good as napster for the deaf"];
+    let landingText = ["the gambling site for children", "it's like github for lesbians", "the child of myspace and wordle", "as good as napster for the deaf", "a rudimentary summoning ritual for maxwell's demon"];
     res.send({"responses": landingText});
 });
 
